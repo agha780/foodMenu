3ca297a7c74626337e6dd82243906186fe67d1c3
John Doe <agha780@gmail.com>
2023-03-20 23:04:59 +0300

    styling got way better using CSS

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   const [totalPrice, setTotalPrice] = useState(0);
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [counterCart, setCounterCart] = useState(0);
+
   const toggleCart = () => {
     setIsCartOpen(!isCartOpen);
   };
@@ -35,19 +36,39 @@ function App() {
       setCartItems(newCartItems);
       setTotalPrice(totalPrice + item.price);
     }
-    setIsCartOpen(true);
+    // setIsCartOpen(true);
     setCounterCart(counterCart + 1);
   };
 
   const removeFromCart = (index) => {
     const itemToRemove = cartItems[index];
-    const newCartItems = [...cartItems];
-    newCartItems.splice(index, 1);
-    setCartItems(newCartItems);
-    setTotalPrice(totalPrice - itemToRemove.price * itemToRemove.quantity);
-    // Thats something im proud of, i mange to update the counter regarding how many itemToRemove.quantity.
-    setCounterCart(counterCart - 1 * itemToRemove.quantity);
+    // Check if the item.quantity is bigger than 1
+    // then bring all the arrays store them into newcartItem
+    // then - = 1 to newCartItem, and then update the state.
+    if (itemToRemove.quantity > 1) {
+      const newCartItems = [...cartItems];
+      newCartItems[index].quantity -= 1;
+      setCartItems(newCartItems);
+      setTotalPrice(totalPrice - itemToRemove.price);
+      setCounterCart(counterCart - 1);
+    } else {
+      const newCartItems = cartItems.filter((item, i) => i !== index);
+      setCartItems(newCartItems);
+      setTotalPrice(totalPrice - itemToRemove.price);
+      setCounterCart(counterCart - 1);
+    }
   };
+
+  // old remove function that delete all the quantity
+  // const removeFromCart = (index) => {
+  //   const itemToRemove = cartItems[index];
+  //   const newCartItems = […cartItems];
+  //   newCartItems.splice(index, 1);
+  //   setCartItems(newCartItems);
+  //   setTotalPrice(totalPrice - itemToRemove.price * itemToRemove.quantity);
+  //   itemToRemove.quantity.
+  //   setCounterCart(counterCart - 1 * itemToRemove.quantity);
+  // };
 
   return (
     <div className="App">
